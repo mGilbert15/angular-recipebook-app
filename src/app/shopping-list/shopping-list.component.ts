04f6f58dad9d9203e6627640f9b051b9f7373a32
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-01 22:20:00 -0400

    create-app-components: create ingredient model and display ingredients in shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients = [new Ingredient('Tomatoes', 2), new Ingredient('Lettece', 1)];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
