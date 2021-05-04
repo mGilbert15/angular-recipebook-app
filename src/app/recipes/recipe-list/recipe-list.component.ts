08334edf493661c0f2ec03e48a419b24b9593e99
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-04 15:11:08 -0400

    pass-recipe-data-with-property-binding: have recipe detail component appear when recipe item is selected

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -18,7 +18,19 @@ export class RecipeListComponent implements OnInit {
       'https://images.squarespace-cdn.com/content/v1/5ade3e0e89c172449f88ac65/1614124886357-MIH6ZYZGVXUCOATZDL4Z/ke17ZwdGBToddI8pDm48kO4Rzin9hdjjWyHGKsueu-J7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0hHMyhIh2kKzuOL3ydJCryDQx51w8Xr20qkny1EoJYIDiHvzfsogRKyWSKFKWODDmQ/Walnut+Cookies.jpg?format=2500w'
     ),
   ];
+
+  //Event emitter for selectedRecipe
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  /**
+   * Emits recipe component was selected to parent component
+   * @param recipe the recipe that was selected
+   */
+  onSelectedRecipeItem(recipe: Recipe) {
+    this.selectedRecipe.emit(recipe);
+  }
 }
