3afa035abdcae9d0643cdeac992b9569820c36d3
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-05 12:12:39 -0400

    create-service-files: add recipes to recipe service and in recipe list component retrieve recipes from service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,30 +1,22 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Pasta Primavera',
-      'A spring pasta dish.',
-      'https://pixnio.com/free-images/2017/03/25/2017-03-25-09-34-43.jpg'
-    ),
-    new Recipe(
-      'Snickerdoodles',
-      'Cinnamon sweet cookies.',
-      'https://images.squarespace-cdn.com/content/v1/5ade3e0e89c172449f88ac65/1614124886357-MIH6ZYZGVXUCOATZDL4Z/ke17ZwdGBToddI8pDm48kO4Rzin9hdjjWyHGKsueu-J7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0hHMyhIh2kKzuOL3ydJCryDQx51w8Xr20qkny1EoJYIDiHvzfsogRKyWSKFKWODDmQ/Walnut+Cookies.jpg?format=2500w'
-    ),
-  ];
+  recipes: Recipe[];
 
   //Event emitter for selectedRecipe
   @Output() selectedRecipe = new EventEmitter<Recipe>();
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   /**
    * Emits recipe component was selected to parent component
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,24 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Pasta Primavera',
+      'A spring pasta dish.',
+      'https://pixnio.com/free-images/2017/03/25/2017-03-25-09-34-43.jpg'
+    ),
+    new Recipe(
+      'Snickerdoodles',
+      'Cinnamon sweet cookies.',
+      'https://images.squarespace-cdn.com/content/v1/5ade3e0e89c172449f88ac65/1614124886357-MIH6ZYZGVXUCOATZDL4Z/ke17ZwdGBToddI8pDm48kO4Rzin9hdjjWyHGKsueu-J7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0hHMyhIh2kKzuOL3ydJCryDQx51w8Xr20qkny1EoJYIDiHvzfsogRKyWSKFKWODDmQ/Walnut+Cookies.jpg?format=2500w'
+    ),
+  ];
+
+  /**
+   * Gets an exact copy of the Recipes stored in the service.
+   * @returns an exact copy of the recipes array
+   */
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
