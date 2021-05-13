a488a72d4b1c9751c00c04d3668d2fca578ba1c8
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-13 09:24:37 -0400

    add-routing: clean up old code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-recipebook-app';
   display = 'recipes';
-
-  /**
-   * Event listener for navigation component changeDisplay event.
-   * @param view the view the app should show
-   */
-  onChangeDisplay(view) {
-    this.display = view;
-  }
 }
