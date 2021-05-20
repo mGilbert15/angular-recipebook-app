16c8768826ea4d65853004f47aad4ed31fe3727e
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-20 13:38:56 -0400

    add-an-authentication-page: created a submittable form on the authentication page

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   isLoginMode: boolean = true;
+  formData: FormGroup;
+
+  ngOnInit() {
+    this.formData = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    console.log(this.formData.value);
+    this.formData.reset();
   }
 }
