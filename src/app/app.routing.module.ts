eb269afd0a837fc66901f9168b7798bd1878f306
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-20 13:13:32 -0400

    add-an-authentication-page: added an auth page and route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from '../app/recipes/recipes.component';
 import { ShoppingListComponent } from '../app/shopping-list/shopping-list.component';
+import { AuthComponent } from './auth/auth.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -39,6 +40,10 @@ const appRoutes: Routes = [
   {
     path: 'shopping-list',
     component: ShoppingListComponent,
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
   },
 ];
 
