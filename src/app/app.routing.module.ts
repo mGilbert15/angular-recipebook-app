0f11edabadcea2c178a929f0582e3b7986e2cee7
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-21 14:18:50 -0400

    add-an-authentication-page: add auth guard to recipes route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from '../app/recipes/recipes.component';
 import { ShoppingListComponent } from '../app/shopping-list/shopping-list.component';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
@@ -16,6 +17,7 @@ const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
