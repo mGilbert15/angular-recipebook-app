fd17ca883888dcfdef0b2b53725a86d063d9d5a1
mGilbert15 <mgilbert@consultwithcase.com>
2021-05-21 13:06:06 -0400

    add-an-authentication-page: added ability to auto login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-recipebook-app';
   display = 'recipes';
+
+  constructor(private authService: AuthService) {}
+
+  /**
+   * Logs user in if token is still valid.
+   */
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -77,6 +77,28 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   /**
    * Logs out user and navigates back to authentication view.
    */
@@ -101,6 +123,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 10000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   /**
